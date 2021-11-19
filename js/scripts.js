88ddc5d36ca331e2eb7c8825cce66bd5811ba77c
Nathan Conn <nconn34@gmail.com>
2021-11-19 13:29:05 -0800

    Wrote function for helloNeighbor, passed first test.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function helloNeighbor(number){
+  const hello3 = "Won't you be my neighbor?";
+  let roboger = "";
+
+  if (number === 3){
+    roboger = hello3;
+    console.log("howdy");
+    return roboger
+  }
+};
