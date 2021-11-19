5f09b58f4719f0aa85b65be682ad9858eaeae053
Nathan Conn <nconn34@gmail.com>
2021-11-19 13:41:20 -0800

    Passed third test

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function helloNeighbor(number){
   const hello3 = "Won't you be my neighbor?";
   const hello2 = "Boop!"
+  const hello1 = "Beep!"
   let roboger = "";
 
   if (number === 3){
@@ -9,6 +10,10 @@ function helloNeighbor(number){
     return roboger
   } else if (number === 2){
       roboger = hello2;
+      console.log("howdy");
+      return roboger
+  } else if (number === 1){
+      roboger = hello1;
       console.log("howdy");
       return roboger
   }
