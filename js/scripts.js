65942c50544b7cd801cc3e55a89703985e066599
Nathan Conn <nconn34@gmail.com>
2021-11-25 14:33:28 -0800

    Cleaned up whitespace

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,6 @@ function helloNeighbor(number){
     roboger = hello1;
     return roboger
 }
-
 };
 function howdyRoboger(number){
   let output = [];
@@ -33,10 +32,8 @@ function howdyRoboger(number){
   howdy2= howdy.replaceAll(3, "Won't you be my neighbor?").replaceAll(2, "Boop!").replaceAll(1, "Beep!")
   return howdy2
 }
-}
-
+};
 //User Interface
-
 $(document).ready(function() {
   $("form#entry").submit(function(event){
     event.preventDefault()
